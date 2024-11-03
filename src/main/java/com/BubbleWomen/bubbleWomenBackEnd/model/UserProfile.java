9326cd41f4c6a04dd9a6e82b4d5cecfc89744112
Pradeesh333 <pradeesh.3333@gmail.com>
2024-11-03 22:35:43 +0530

    removed key

---
+++ src/main/java/com/BubbleWomen/bubbleWomenBackEnd/model/UserProfile.java
@@ -0,0 +1,8 @@
+package com.BubbleWomen.bubbleWomenBackEnd.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "userProfile")
+public class UserProfile {
+
+}
